5c642543f7ad20d00a5a31a71350e6a3cf9f415c
svermen <svermen@gmail.com>
2021-08-11 15:16:05 -0300

    Updates

---
+++ precache-manifest.184e80154ee3d56cb57161b1d26461fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "897553039ab6a184f75b",
+    "url": "/fundom-prueba/static/css/main.605b16ab.chunk.css"
+  },
+  {
+    "revision": "897553039ab6a184f75b",
+    "url": "/fundom-prueba/static/js/main.89755303.chunk.js"
+  },
+  {
+    "revision": "0905d4db1db6c0cdcd70",
+    "url": "/fundom-prueba/static/css/1.410082d2.chunk.css"
+  },
+  {
+    "revision": "0905d4db1db6c0cdcd70",
+    "url": "/fundom-prueba/static/js/1.0905d4db.chunk.js"
+  },
+  {
+    "revision": "2f5eac9c7514940371ec",
+    "url": "/fundom-prueba/static/js/runtime~main.2f5eac9c.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/fundom-prueba/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "08d32285cfb4354721717396c4729ae3",
+    "url": "/fundom-prueba/index.html"
+  }
+];
